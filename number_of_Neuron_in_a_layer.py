7d69ab3c1228f10783adb588b889a4673ccd8cd6
Weng-john <59682713+Weng-john@users.noreply.github.com>
2020-10-10 00:50:56 +0800

    Create number_of_Neuron_in_a_layer.py

---
+++ number_of_Neuron_in_a_layer.py
@@ -0,0 +1,76 @@
+%env KERAS_BACKEND=tensorflow
+
+
+from keras.datasets import mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import glob
+
+from keras.utils import np_utils
+
+
+import cv2
+from google.colab.patches import cv2_imshow
+
+from sklearn.model_selection import train_test_split
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.optimizers import SGD
+
+
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+y_train = np_utils.to_categorical(y_train,10)
+y_test = np_utils.to_categorical(y_test, 10)
+
+r = 0
+Round = 20
+Times = 28
+tmpAccuracy = [0]*Round
+Accuracy = [0]*Times
+for nodeNum in range(20,(Times*10+20),10):
+  sum = 0
+  for ro in range(1,Round+1):
+    model_2 = Sequential()
+    model_2.add(Conv2D (input_shape=(28,28,1), filters=4, kernel_size=(3,3), padding='same', activation='relu'))
+    model_2.add(MaxPooling2D(pool_size=(2,2)))
+    model_2.add(Flatten())
+
+    model_2.add(Dense(units=nodeNum, kernel_initializer='normal', activation='relu'))
+
+    model_2.add(Dense(units=10, kernel_initializer='normal', activation='softmax'))
+
+
+    model_2.summary()
+    model_2.compile(loss='mse', optimizer=SGD(lr=0.1), metrics=['accuracy'])
+
+    model_2.fit(x_train, y_train, epochs=12, batch_size=200, verbose=2)
+
+    scores = model_2.evaluate(x_test, y_test)
+    tmpAccuracy[ro-1] = float(scores[1]*100.0)
+    print("\n\t[INFO] Accuracy of testing data = {:2.1f}%".format(scores[1]*100.0))
+    print()
+  for i in range(Round-1,0,-1):
+    for j in range(0,i-1):
+      if tmpAccuracy[j]>tmpAccuracy[j+1]:
+        tmp = tmpAccuracy[j]
+        tmpAccuracy[j] = tmpAccuracy[j+1]
+        tmpAccuracy[j+1] = tmp
+  midAccuracy = (tmpAccuracy[int(Round/2)]+tmpAccuracy[int(Round/2+1)])/2
+  for i in range(0,Round):
+    if (abs(tmpAccuracy[i]-midAccuracy)/midAccuracy)>10:
+      tmpAccuracy.remove(tmpAccuracy[i])
+      i-=1
+      Round-=1
+  for i in range(0,Round):
+    sum += tmpAccuracy[i]
+  Accuracy[r] = float(sum / float(Round))
+  r+=1
+  Round=20
+
+for i in range(0,Times):
+  print("\tAccuracy of Round are = {:2.1f}%".format(Accuracy[i]))
